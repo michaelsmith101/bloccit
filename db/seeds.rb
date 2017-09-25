96e8a869cc773dea267510353c694bee9a65ecde
Michael Smith <michaelsmith101@gmail.com>
2017-09-25 05:12:14 +0000

    Completed Checkpoint 28

--- db/seeds.rb
+++ db/seeds.rb
@@ -39,10 +39,19 @@ topics = Topic.all
    )
  end
  
- user = User.first
- user.update_attributes!(
-   email: 'test', # replace this with your personal email
-   password: 'password'
+ # Create an admin user
+ admin = User.create!(
+   name:     'Admin User',
+   email:    'admin@example.com',
+   password: 'helloworld',
+   role:     'admin'
+ )
+ 
+ # Create a member
+ member = User.create!(
+   name:     'Member User',
+   email:    'member@example.com',
+   password: 'helloworld'
  )
  
  puts "Seed finished"
