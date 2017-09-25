6e392aedf78d18babf74a027afe8876cdd832328
Michael Smith <michaelsmith101@gmail.com>
2017-09-25 04:28:32 +0000

    Completed Checkpoint 27

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,6 +2,9 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
    let(:user) { User.create!(name: "Bloccit User", email: "user@bloccit.com", password: "password") }
+   
+   it { is_expected.to have_many(:posts) }
+   
    # Shoulda tests for name
    it { is_expected.to validate_presence_of(:name) }
    it { is_expected.to validate_length_of(:name).is_at_least(1) }
@@ -21,6 +24,7 @@ RSpec.describe User, type: :model do
      it "should have name and email attributes" do
        expect(user).to have_attributes(name: "Bloccit User", email: "user@bloccit.com")
      end
+
    end
     
     describe "invalid user" do
@@ -37,5 +41,7 @@ RSpec.describe User, type: :model do
         end
  
    end
+   
+   
 
 end
